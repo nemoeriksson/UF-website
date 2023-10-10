6627e6bd5a1e9328e0a76be142e2bd1d46e38447
nemoeriksson <nemoeriksson2005@gmail.com>
2023-10-10 11:35:56 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="/style/homepage.css">
+
+<script lang="ts">
+
+</script>
+
+<nav>
+    <span class="companyName">Screaming Plant UF</span>
+    <div class="links">
+        <a href="#">Our Products</a>
+        <a href="#">Login</a>
+        <a href="#">Register</a>
+    </div>
+</nav>
+
+<div class="welcomeSection">
+    <div class="contentContainer">
+        <p class="titel">Lorem, ipsum dolor.</p>
+        <p class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident maxime tenetur laboriosam.</p>
+    </div>
+    <div class="scrollDownIcon"></div>
+</div>
